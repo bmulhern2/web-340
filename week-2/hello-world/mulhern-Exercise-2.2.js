4c95ac05a6d5bc672148440a60ab1bd5aae24a31
bmulhern2 <bmulhern09@gmail.com>
2020-02-25 10:50:58 -0500

    commit

---
+++ week-2/hello-world/mulhern-Exercise-2.2.js
@@ -0,0 +1,18 @@
+/*
+Title: Exercise 2.2
+Author: Professor Krasso
+Date: 25 Feburary 2020
+Modified By: Brendan Mulhern
+Description: This is an HTTP server built with HTTP and Express
+*/
+var express = require('express');
+var http = require('http');
+
+var app = express();
+
+app.use(function(request, response){
+    console.log("In comes a request to:" + request.url);
+    response.end("Hello World");
+});
+
+http.createServer(app).listen('8080');
