db77582e07fcef9b991cc9232abbb0b88d770718
bmulhern2 <bmulhern09@gmail.com>
2020-03-04 09:20:57 -0500

    commit

--- week-3/advanced-routing/mulhern-Exercise-3.3.js
+++ week-3/advanced-routing/mulhern-Exercise-3.3.js
@@ -5,3 +5,19 @@ Date: 4 March 2020
 Modified By: Brendan Mulhern
 Description: This is outputting the employee ID.
 */
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
+
+var app = express();
+app.set("views", path.resolve(__dirname + "/views"));
+app.set('view engine', 'ejs');
+app.use(logger("short"));
+app.get('/:employeeID', (req, res) => {
+    var employeeID = parseInt(req.params.employeeID, 10);
+    res.render('index', { employeeID: employeeID });
+});
+http.createServer(app).listen(1000, () => {
+    console.log("Server Started on Port 1000");
+})
